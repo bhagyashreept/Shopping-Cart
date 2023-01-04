ced7bc39a04403e518ed6d330da5d5204e869cf7
bhagyashreept <55779449+bhagyashreept@users.noreply.github.com>
2023-01-05 00:37:29 +0530

    incrementcartcount

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import app from "./App.module.css";
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+  const [cartItems, setCartItems] = useState([]);
+  const addInCart = (itemid) => {
+    setCartCount(cartCount + 1);
+    setCartItems([...cartItems, itemid]);
+  };
   const getAPIData = async () => {
     const res = await axios.get("https://dummyjson.com/products");
-    console.log(res.data.products);
+    // console.log(res.data.products);
     setProducts(res.data.products);
   };
   useEffect(() => {
@@ -21,30 +27,35 @@ const App = () => {
   // ];
 
   return (
-    <div className={app.divCSS}>
-      {products.map((item) => (
-        <div key={item.id}>
-          <img
-            className={app.imgCSS}
-            style={{
-              objectFit: "cover"
-            }}
-            height="200px"
-            width="200px"
-            src={item.thumbnail}
-            alt={item.title}
-          ></img>
-          <div className={app.titlepriceDiv}>
-            <span>{item.title}</span>
-            <b>$ {item.price}</b>
+    <>
+      <button>Cart - {cartCount}</button>
+      <div className={app.divCSS}>
+        {products.map((item) => (
+          <div key={item.id}>
+            <img
+              className={app.imgCSS}
+              style={{
+                objectFit: "cover"
+              }}
+              height="200px"
+              width="200px"
+              src={item.thumbnail}
+              alt={item.title}
+            ></img>
+            <div className={app.titlepriceDiv}>
+              <span>{item.title}</span>
+              <b>$ {item.price}</b>
+            </div>
+            <div className={app.btndiv}>
+              <button onClick={addInCart} className={app.toCartBtn}>
+                Add to Cart
+              </button>
+              <button className={app.buyNowBtn}>Buy Now</button>
+            </div>
           </div>
-          <div className={app.btndiv}>
-            <button className={app.toCartBtn}>Add to Cart</button>
-            <button className={app.buyNowBtn}>Buy Now</button>
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
